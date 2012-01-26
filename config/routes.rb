a6ab5ab307b7a5c7be072c322fc4d17a4ee1b5be
kailash kumar <kailashk06@gmail.com>
2012-01-26 18:05:29 +0530

    depotcatalog checkout complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Depotcatalog::Application.routes.draw do
+  resources :orders
+
   resources :line_items
 
   resources :carts
@@ -6,6 +8,10 @@ Depotcatalog::Application.routes.draw do
   get "store/index"
 
   resources :products
+  
+  resources :products do
+    get :who_bought, :on => :member
+  end
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -56,7 +62,7 @@ Depotcatalog::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-   root :to => 'store#index'
+   root :to => 'store#index', :as => 'store'
 
   # See how all your routes lay out with "rake routes"
 
