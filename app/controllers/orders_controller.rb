57524411214a0cb065b8fca85938a8925fd36380
kailash kumar <kailashk06@gmail.com>
2012-01-27 01:46:46 +0530

    depotcatalog logging

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,5 @@
 class OrdersController < ApplicationController
+  skip_before_filter :authorize, :only => [:new, :create]
   # GET /orders
   # GET /orders.json
   def index
@@ -53,7 +54,7 @@ class OrdersController < ApplicationController
       if @order.save
         Cart.destroy(session[:cart_id])
         session[:cart_id] = nil
-      
+      #  Notifier.order_received(@order).deliver
         format.html { redirect_to store_url, notice: 'Thank you for your Order.' }
         format.json { render json: @order, status: :created, location: @order }
       else
